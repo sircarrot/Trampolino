ee417f7794dca50718bd481378ad428cf97d8c3e
Alvin Tjhie <alvinwiguna@yahoo.com>
2016-08-20 10:47:54 +0800

    Pause Script

---
+++ Assets/Alvin/Scripts/SinglePlayer/Pause.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    public bool paused;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void pause()
+    {
+        if(paused)
+        {
+            Time.timeScale = 1.0f;
+            paused = false;
+        }
+        
+        else
+        {
+            Time.timeScale = 0.0f;
+            paused = true;
+        }
+
+    }
+    public void unPause()
+    {
+        
+    }
+
+}
